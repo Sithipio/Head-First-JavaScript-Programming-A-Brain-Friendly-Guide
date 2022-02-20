3b2d443287280fb99b24ef473657c4ee237ed9c1
Sithipio <vberezos@gmail.com>
2022-02-20 13:02:28 +0200

    Continue learning 20.02.2022
    Chapter 2

--- chapter 2/battleship.js
+++ chapter 2/battleship.js
@@ -1,6 +1,39 @@
-Math.floor(Math.random() * (max - min + 1)) + min
-function randomPosition(myMin, myMax) {
+/*function randomPosition(myMin, myMax) {
   return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin
 }
+
   var randomPositionShip = randomPosition(0, 6);
-  console.log(
+ document.write(randomPositionShip);
+*/
+
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+
+var guess;
+var hits = 0;
+var guesses = 0;
+
+var isSunk = false;
+
+while (isSunk == false) {
+	guess = prompt("Ready, aim, fire! (enter a number 0-6):");
+	if (guess < 0 || guess > 6) {
+		alert("Please enter a valid cell number!");
+	}
+	else {
+		guesses = guesses + 1;
+		if ( guess == location1 || guess == location2 || guess == location3){
+			hits = hits + 1;
+			alert("HIT!");
+			if (hits == 3) {
+				isSunk = true;
+				alert("You sank my battleship!");
+				}
+			} else {
+				alert("MISS!");
+			}
+		}
+	}
+var stats = "You took " + guesses + " guesses to sink the battleship, " + "whitch means your shoting accuracy was " + (3/guesses);
+alert(stats);
