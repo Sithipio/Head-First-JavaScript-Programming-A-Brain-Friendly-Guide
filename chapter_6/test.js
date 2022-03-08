166cc2cc5c02da320129bd480f873c3fc5bddd05
Sithipio <vberezos@gmail.com>
2022-03-08 15:17:17 +0200

    Continue learning 08.03.2022
    Chapter 6

---
+++ chapter_6/test.js
@@ -0,0 +1,8 @@
+function init () {
+        let textForH1 = document.getElementById("main");
+        textForH1.setAttribute("class", "redtext");
+        console.log(textForH1);
+}
+
+window.onload = init;
+
